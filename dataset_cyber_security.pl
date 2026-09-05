86802426f113c105655631ca2652e96cc6a524de
Lokin Pendawa <lokinpendawa@gmail.com>
2026-09-06 02:38:56 +0700

    Rename accumulated_knowledge_base to knowledge_base

--- dataset_cyber_security.pl
+++ dataset_cyber_security.pl
@@ -1,8 +1,8 @@
 :- dynamic accumulated_knowledge_base/4.
 
-accumulated_knowledge_base(cyber_defense, slow_http_dos, 'ABSOLUTE VERIFIED', 'Slow HTTP DoS (RUDY/Slowloris) Connection Exhaustion Attack').
-accumulated_knowledge_base(cyber_defense, zero_day_exploit, 'ABSOLUTE VERIFIED', 'Kernel-Space Zero-Day Payload Exploit Attempt').
-accumulated_knowledge_base(cyber_defense, redos_attack, 'ABSOLUTE VERIFIED', 'Regular Expression Denial of Service (ReDoS) CPU Exhaustion').
-accumulated_knowledge_base(cyber_defense, low_slow_sqli, 'ABSOLUTE VERIFIED', 'Low-and-Slow Time-Based Blind SQL Injection').
-accumulated_knowledge_base(cyber_defense, ddos_incident, 'ABSOLUTE VERIFIED', 'Layer-7 Traffic Flooding and Exhaustion Attack').
+knowledge_base(cyber_defense, slow_http_dos, 'ABSOLUTE VERIFIED', 'Slow HTTP DoS (RUDY/Slowloris) Connection Exhaustion Attack').
+knowledge_base(cyber_defense, zero_day_exploit, 'ABSOLUTE VERIFIED', 'Kernel-Space Zero-Day Payload Exploit Attempt').
+knowledge_base(cyber_defense, redos_attack, 'ABSOLUTE VERIFIED', 'Regular Expression Denial of Service (ReDoS) CPU Exhaustion').
+knowledge_base(cyber_defense, low_slow_sqli, 'ABSOLUTE VERIFIED', 'Low-and-Slow Time-Based Blind SQL Injection').
+knowledge_base(cyber_defense, ddos_incident, 'ABSOLUTE VERIFIED', 'Layer-7 Traffic Flooding and Exhaustion Attack').
 
